16ea776f7ff8aa86da79d418d029dec64409ffc5
NimbusNiles <nielspetterson@gmail.com>
2017-07-17 21:59:11 +0200

    Refactored ActionMaster to be a static class without state

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -2,66 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionMaster {
+public static class ActionMaster {
 
     public enum BowlAction {Tidy, Reset, EndTurn, EndGame };
 
-    private int[] bowls = new int[21];
-    private int bowl = 1;
-
     public static BowlAction NextAction(List<int> bowls) {
-        ActionMaster actionMaster = new ActionMaster();
+        
         BowlAction NextBowlAction = new BowlAction();
+        
+        int bowl = 1;
 
-        foreach(int bowl in bowls) {
-            NextBowlAction = actionMaster.Bowl(bowl);
+        for (int ii = 0; ii < bowls.Count; ii++) {
+
+            int pins = bowls[ii];
+
+            if (pins < 0 || pins > 10) { throw new UnityException("Impossible number of pins recorded: " + pins); }
+
+            if (bowl == 21) {
+                NextBowlAction = BowlAction.EndGame;
+                continue;
+            }
+
+            if ((bowl == 19 || bowl == 20) && pins == 10) {
+                bowl += 1;
+                NextBowlAction = BowlAction.Reset;
+                continue;
+            }
+
+            if (bowl == 20 && bowls[ii - 1] == 10) {
+                bowl += 1;
+                NextBowlAction = BowlAction.Tidy;
+                continue;
+            }
+
+            if (bowl == 20) {
+                int lastAndThisBowlTotal = bowls[ii - 1] + bowls[ii];
+                if (lastAndThisBowlTotal == 10) {
+                    bowl += 1;
+                    NextBowlAction = BowlAction.Reset;
+                    continue;
+                } else {
+                    NextBowlAction = BowlAction.EndGame;
+                    continue;
+                }
+            }
+            
+            if (pins == 10 && bowl % 2 != 0) {
+                bowl += 2;
+                NextBowlAction = BowlAction.EndTurn;
+                continue;
+            }
+
+            if (bowl % 2 != 0) {
+                bowl += 1;
+                NextBowlAction = BowlAction.Tidy;
+                continue;
+            } else if (bowl % 2 == 0) {
+                bowl += 1;
+                NextBowlAction = BowlAction.EndTurn;
+                continue;
+            }
+
+            throw new UnityException("Not sure what action to return.");
+
         }
 
         return NextBowlAction;
-    }
-
-	private BowlAction Bowl (int pins) {
-        if (pins < 0 || pins > 10) {throw new UnityException("Impossible number of pins recorded: " + pins); }
-
-        bowls[bowl - 1] = pins;
-
-        if (bowl == 21) {
-            return BowlAction.EndGame;
-        }
-
-        if ((bowl == 19 || bowl == 20) && pins == 10){
-            bowl += 1;
-            return BowlAction.Reset;
-        }
-
-        if (bowl == 20 && bowls[bowl-2] == 10) {
-            bowl += 1;
-            return BowlAction.Tidy;
-        }
-
-        if (bowl == 20) {
-            int lastAndThisBowlTotal = bowls[bowl - 2] + bowls[bowl - 1];
-            if (lastAndThisBowlTotal == 10) {
-                bowl += 1;
-                return BowlAction.Reset;
-            } else {
-                return BowlAction.EndGame;
-            }
-        }
-
-        if (pins == 10 && bowl % 2 != 0) {
-            bowl += 2;
-            return BowlAction.EndTurn;
-        }
-
-        if (bowl % 2 != 0) {
-            bowl += 1;
-            return BowlAction.Tidy;
-        } else if (bowl % 2 == 0) {
-            bowl += 1;
-            return BowlAction.EndTurn;
-        }
-
-        throw new UnityException("Not sure what action to return.");
     }
 }
